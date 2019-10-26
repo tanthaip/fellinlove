90e4627d6e397c7950b41a27756210014813bd73
Megan <meganhu@berkeley.edu>
2019-10-26 14:26:48 -0700

    added new questions

--- javascript.js
+++ javascript.js
@@ -1,22 +1,27 @@
-var questions1 = [
-    '10 seconds have not elapsed',
-    '10 seconds have not elapsed!',
-    '10 seconds have not elapsed!!',
-    '10 seconds have not elapsed!!!'
+var sweet = [
+    "Would you like to be famous? In what way?",
+    "What's your favourite way to waste time?",
+    "What would constitute a 'perfect' day for you?",
+    "When did you last sing to yourself? To someone else?",
+    "If you were able to live to the age of 90 and retain either the mind or body of a 30-year old, which would you want?"
 ]
 
-var questions2 =  [
-    'over 20 seconds have elapsed',
-    'over 20 seconds have elapsed!',
-    'over 20 seconds have elapsed!!',
-    'over 20 seconds have elapsed!!!'
+var sour =  [
+    "Take four minutes and tell your partner your life story in as much detail as possible.",
+    "For what in your life do you feel most grateful?",
+    "What does your ideal life look like?",
+    "If you could wake up tomorrow having gained any one quality or ability, what would it be?",
+    "Do you have a secret hunch about how you will die?",
+    "What is the greatest accomplishment of your life?"
 
 ]
-var questions3 =  [
-    'over 30 seconds have elapsed',
-    'over 30 seconds have elapsed!',
-    'over 30 seconds have elapsed!!',
-    'over 30 seconds have elapsed!!!'
+var salty =  [
+    "What has been your greatest challenge, and how did you overcome it?",
+    "What is your most terrible memory?",
+    "Would you rather be crazy rich, or deeply in love?",
+    "How do you feel about your relationship with your mother?",
+    "What's one thing that you would change on yourself if you could?",
+    "What roles do love and affection play in your life?"
 ]
 
 var savory [
@@ -50,9 +55,9 @@ var umami [
 ]
 
 
-var used1 = [];
-var used2 = [];
-var used3 = [];
+var sweet_used = [];
+var sour_used = [];
+var salty_used = [];
 var savory_used = [];
 var smoky_used = [];
 var spicy_used = [];
@@ -75,35 +80,35 @@ function nextQuestion() {
     var timeElapsed = Date.now() - startTime;
     console.log(timeElapsed)
 
-    if (timeElapsed < 10000 && used1.length !== questions1.length) {
-        return inner_helper(questions1, used1)
+    if (timeElapsed < 10000 && sweet_used.length !== sweet.length) {
+        return inner_helper(sweet, sweet_used)
         // randomNumber = Math.floor(Math.random() * (questions1.length))
         // while (used1.includes(randomNumber)) {
         //     randomNumber = Math.floor(Math.random() * (questions1.length))
         // }
         // document.getElementById('question').innerHTML = questions1[randomNumber];
         // used1.push(randomNumber);
-        console.log(used1);
+        console.log(sweet_used);
 
-    } else if (timeElapsed < 15000 && used2.length !== questions2.length) {
-        return inner_helper(questions2, used2)
+    } else if (timeElapsed < 15000 && sour_used.length !== sour.length) {
+        return inner_helper(sour, sour_used)
         // randomNumber = Math.floor(Math.random() * (questions2.length))
         // while (used2.includes(randomNumber)) {
         //     randomNumber = Math.floor(Math.random() * (questions2.length))
         // }
         // document.getElementById('question').innerHTML = questions2[randomNumber];
         // used2.push(randomNumber);
-        // console.log(used2);
+        console.log(sour_used);
         
-    } else if (timeElapsed < 25000 && used3.length !== questions3.length) {
-        return inner_helper(questions3, used3)
+    } else if (timeElapsed < 25000 && salty_used.length !== salty.length) {
+        return inner_helper(salty, salty_used)
         // randomNumber = Math.floor(Math.random() * (questions3.length))
         // while (used3.includes(randomNumber)) {
         //     randomNumber = Math.floor(Math.random() * (questions3.length))
         // }
         // document.getElementById('question').innerHTML = questions3[randomNumber];
         // used3.push(randomNumber);
-        // console.log(used3);
+        console.log(salty_used);
     } if (timeElapsed < 30000 && used1.length !== savory.length) {
         return inner_helper(savory, savory_used)
 
